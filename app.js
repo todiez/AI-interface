8439487dfaec3f7efad0eb85fb40dd67b7920745
todiez <tobias@diez.guru>
2023-06-16 16:18:21 +0200

    api request added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("it is running...")
+const readline = require("readline");
+const { generateMeta } = require("./controllers/openaiController")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("YouTube Video Title: \n", generateMeta);
